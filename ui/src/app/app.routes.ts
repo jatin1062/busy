ad7ca2b97d13c5b5759104c5dc299889b8f9a471
jatin1062 <jatin.verma009@gmail.com>
2024-10-29 18:22:55 +0530

    first commit ui, express and python

---
+++ ui/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+
+
+
+  export const routes: Routes = [{
+    path: 'home',
+    component: HomepageComponent,
+    title: 'Home page'
+  }];
+
